6024edf4a2fac3ced9a3cb2f98640b434b5fb1c1
dmrfrht <dmrfrht1@gmail.com>
2021-02-21 13:32:40 +0300

    profile design

--- router/user/controller.js
+++ router/user/controller.js
@@ -1,29 +1,30 @@
 const model = require('./model')
+const postModel = require('../post/model')
 const jwt = require('jsonwebtoken')
 const config = require('../../config')
 
 module.exports = {
   login: (req, res) => {
-    model.findOne({ email: req.body.email }, (err, user) => {
+    model.findOne({email: req.body.email}, (err, user) => {
       if (err) {
-        res.status(500).send({ auth: false, msg: err })
+        res.status(500).send({auth: false, msg: err})
         return
       }
 
       if (!user) {
-        res.send({ auth: false, mailError: true, msg: "Email not found" })
+        res.send({auth: false, mailError: true, msg: "Email not found"})
         return
       }
 
       user.comparePassword(req.body.password, (err, isMatch) => {
         if (err) throw err
         if (isMatch) {
-          let token = jwt.sign({ id: user._id }, config.secret, { expiresIn: 86400 })
-          res.status(200).send({ auth: true, token })
+          let token = jwt.sign({id: user._id}, config.secret, {expiresIn: 86400})
+          res.status(200).send({auth: true, token})
 
           return
         } else {
-          res.send({ auth: false, passError: true, msg: "Password is incorrect" })
+          res.send({auth: false, passError: true, msg: "Password is incorrect"})
         }
       })
     })
@@ -39,17 +40,35 @@ module.exports = {
 
     newUser.save()
       .then(result => {
-        let token = jwt.sign({ id: result._id }, config.secret, { expiresIn: 86400 })
-        res.status(200).send({ auth: true, token })
+        let token = jwt.sign({id: result._id}, config.secret, {expiresIn: 86400})
+        res.status(200).send({auth: true, token})
       })
       .catch(err => {
         if (err.code === 11000) {
-          res.send({ auth: false, msg: "Email already exists.." })
+          res.send({auth: false, msg: "Email already exists.."})
           return
         }
 
-        res.send({ auth: false, msg: "An internal server error has occurred" })
+        res.send({auth: false, msg: "An internal server error has occurred"})
       })
 
   },
+  getProfile: (req, res) => {
+    let user_id = jwt.decode(req.body.auth_token).id
+    model.findById(user_id)
+      .then(user => {
+        if (!user) res.send({success: false, msg: "User not found"})
+
+        postModel.find({user_id})
+          .then(posts => {
+            res.send({
+              success: true,
+              details: {
+                display_name: user.forename + ' ' + user.surname,
+                posts
+              }
+            })
+          })
+      })
+  }
 }
--- router/user/index.js
+++ router/user/index.js
@@ -3,5 +3,6 @@ const controller = require('./controller')
 
 router.post('/login', controller.login) // host/user/login
 router.post('/register', controller.register)
+router.post('/get-profile', controller.getProfile)
 
 module.exports = router
